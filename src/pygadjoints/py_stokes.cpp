fec032396c1e8e0db2556061a29b7e7feb3f67e4
markriegler <mark.riegler40@gmail.com>
2024-09-16 16:24:43 +0200

    Add Init function

--- src/pygadjoints/py_stokes.cpp
+++ src/pygadjoints/py_stokes.cpp
@@ -27,3 +27,143 @@ void add_stokes_problem(py::module_ &m) {
 #endif
       ;
 }
+
+// StokesProblem functions
+namespace pygadjoints {
+
+void StokesProblem::ReadInputFromFile(const std::string &filename) {
+  const Timer timer("ReadInputFromFile");
+  // IDs in the xml input file (might change later)
+  const index_t mp_id{0}, vel_bc_id{2}, p_bc_id{3}, ass_opt_id{4},
+      vel_ana_id{12}, p_ana_id{13};
+
+  // Import mesh and load relevant information
+  gsFileData<> fd(filename);
+  fd.getId(mp_id, mp_pde);
+
+  // Check if file has analytical solution for velocity and pressure
+  // respectively
+  if (fd.hasId(vel_ana_id)) {
+    fd.getId(vel_ana_id, vel_analytical_solution);
+    has_velocity_solution = true;
+  }
+  if (fd.hasId(p_ana_id)) {
+    fd.getId(p_ana_id, p_analytical_solution);
+    has_pressure_solution = true;
+  }
+
+  // Read boundary conditions for velocity
+  fd.getId(vel_bc_id, vel_bcs);
+  vel_bcs.setGeoMap(mp_pde);
+  // Read pressure boundary conditions if given
+  if (fd.hasId(p_bc_id)) {
+    fd.getId(p_bc_id, p_bcs);
+    has_pressure_bcs = true;
+    p_bcs.setGeoMap(mp_pde);
+  }
+
+  // Read assembly options if given
+  if (fd.hasId(ass_opt_id)) {
+    gsOptionList Aopt;
+    fd.getId(ass_opt_id, Aopt);
+
+    // Set assembly options in the expression assembler
+    expr_assembler_pde.setOptions(Aopt);
+  }
+}
+
+void StokesProblem::Init(const std::string &filename,
+                         const int number_of_refinements,
+                         const int number_degree_elevations,
+                         const bool print_summary) {
+  const Timer timer("Initialization");
+  const index_t pressure_id{0}, velocity_id{1};
+
+  // Process information from input file
+  StokesProblem::ReadInputFromFile(filename);
+
+  // Set the dimension
+  dimensionality_ = mp_pde.geoDim();
+
+  // Set up function basis and refinements
+  velocity_basis = gsMultiBasis<>(mp_pde, true);
+  pressure_basis = gsMultiBasis<>(mp_pde, true);
+
+  // p-refinement
+  // Use Taylor-Hood elements: eleveate the degree of velocity for inf-sup
+  // stability
+  velocity_basis.setDegree(velocity_basis.maxCwiseDegree() +
+                           number_degree_elevations + 1);
+  pressure_basis.setDegree(pressure_basis.maxCwiseDegree() +
+                           number_degree_elevations);
+
+  // h-refinement
+  for (index_t href{0}; href < number_of_refinements; href++) {
+    velocity_basis.uniformRefine();
+    pressure_basis.uniformRefine();
+  }
+
+  // Elements used for numerical integration
+  expr_assembler_pde.setIntegrationElements(velocity_basis);
+  // Get geometry mapping
+  pGeometry_expression =
+      std::make_shared<geometryMap>(expr_assembler_pde.getMap(mp_pde));
+
+  // Set the discretization spaces
+  pVelocity_space = std::make_shared<space>(expr_assembler_pde.getSpace(
+      velocity_basis, dimensionality_, velocity_id));
+  pPressure_space = std::make_shared<space>(
+      expr_assembler_pde.getSpace(pressure_basis, 1, pressure_id));
+
+  // Partial solution vectors
+  pVelocity_solution = std::make_shared<solution>(
+      expr_assembler_pde.getSolution(*pVelocity_space, solVector));
+  pPressure_solution = std::make_shared<solution>(
+      expr_assembler_pde.getSolution(*pPressure_space, solVector));
+
+  // Set up Dirichlet boundary conditions, with C0-continuity
+  pVelocity_space->setup(vel_bcs, dirichlet::l2Projection, 0);
+  if (has_pressure_bcs) {
+    pPressure_space->setup(p_bcs, dirichlet::l2Projection, 0);
+  }
+
+  // Assign DoF mapper to pressure basis (since velocity is elevated in degree)
+  pDof_mapper = std::make_shared<gsDofMapper>(pPressure_space->mapper());
+
+  // Expression evaluator
+  pExpr_evaluator = std::make_shared<gsExprEvaluator<>>(
+      gsExprEvaluator<>(expr_assembler_pde));
+
+  // Initialize the system
+  expr_assembler_pde.initSystem();
+
+  // Print summary of function bases
+  if (print_summary) {
+    std::string padding{"        : "};
+    std::cout << padding << "Simulation summary:\n"
+              << padding << "--------------------\n"
+              << padding << "Number of DOFs :\t\t"
+              << expr_assembler_pde.numDofs() << "\n"
+              << padding << "Number of patches :\t\t" << mp_pde.nPatches()
+              << "\n";
+    std::cout << padding << "Spline degrees (min->max):\n";
+    std::cout << padding << "\tVelocity: \t\t"
+              << velocity_basis.minCwiseDegree() << "->"
+              << velocity_basis.maxCwiseDegree() << '\n';
+    std::cout << padding << "\tPressure: \t\t"
+              << pressure_basis.minCwiseDegree() << "->"
+              << pressure_basis.maxCwiseDegree() << '\n';
+    std::cout << std::endl;
+  }
+}
+
+void StokesProblem::Assemble() { const Timer timer("Assemble"); }
+
+void StokesProblem::ExportParaview(const std::string &fname,
+                                   const bool &plot_elements,
+                                   const int &sample_rate,
+                                   const bool &export_b64) {
+  Timer timer("Exporting Paraview");
+}
+
+} // namespace pygadjoints
--- src/pygadjoints/py_stokes.hpp
+++ src/pygadjoints/py_stokes.hpp
@@ -16,31 +16,66 @@ public:
 
   gsStopwatch timer;
 
+  /// @brief Set up material constants
+  /// @param viscosity Dynamic viscosity in Pa⋅s
+  /// @param density Density in kg/m³
   void SetMaterialConstants(const real_t &viscosity, const real_t &density) {
     density_ = density;
     viscosity_ = viscosity;
   }
 
-  void ExportParaview(const std::string &fname, const bool &plot_elements,
-                      const int &sample_rate, const bool &export_b64) {
-    Timer timer("Exporting Paraview");
-  }
+  /// @brief Read relevant information from file
+  /// @param filename Input xml file
+  void ReadInputFromFile(const std::string &filename);
 
+  /// @brief Initialize geometry and function spaces
+  /// @param filename Filename with geometry, function, boundary conditions and
+  /// assembly definitions
+  /// @param number_of_refinements Number of h-refinements
+  /// @param number_degree_elevations Number of degree elevations
+  /// @param print_summary If true, print a summary of the geometry and function
+  /// spaes
   void Init(const std::string &filename, const int number_of_refinements,
             const int number_degree_elevations,
-            const bool print_summary = false) {
-    const Timer timer("Initialisation");
+            const bool print_summary = false);
 
-    // }
-  }
+  /// @brief Assemble the system matrix and rhs of the Stokes equation
+  void Assemble();
 
-  void Assemble() { const Timer timer("Assemble"); }
-
+  /// @brief Solve linear system of system matrix and system rhs
   void SolveLinearSystem() { PdeProblem::SolveLinearSystem<SolverType>(); }
+
+  /// @brief Exporting the field variables to a ParaView file
+  /// @param fname Output file name
+  /// @param plot_elements If true, plot patch boundaries
+  /// @param sample_rate Samples per element
+  /// @param export_b64 If true, export values in 64-bit binary format
+  void ExportParaview(const std::string &fname, const bool &plot_elements,
+                      const int &sample_rate, const bool &export_b64);
 
 private:
   real_t density_{1};
   real_t viscosity_{1};
+
+  // Indicator if input file has analytical solution(s)
+  bool has_velocity_solution{false}, has_pressure_solution{false};
+
+  // Analytical solutions to velocity and pressure
+  gsFunctionExpr<> vel_analytical_solution{}, p_analytical_solution{};
+
+  // Boundary conditions
+  gsBoundaryConditions<> vel_bcs, p_bcs;
+  bool has_pressure_bcs{false};
+
+  // Function basis for velocity and pressure
+  gsMultiBasis<> velocity_basis, pressure_basis;
+
+  // Discretization spaces
+  std::shared_ptr<space> pVelocity_space{nullptr}, pPressure_space{nullptr};
+
+  // Partial solution values/expression for the variables
+  std::shared_ptr<solution> pVelocity_solution{nullptr},
+      pPressure_solution{nullptr};
 };
 
 } // namespace pygadjoints
