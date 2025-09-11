9edb59cdcb4038784d8082c53e7e23c8b65b213e
markriegler <mark.riegler40@gmail.com>
2025-09-11 18:51:39 +0200

    Evaluate parametrized tile

--- examples/Stokes/ConcentrationOptimization/geometry_kernel.py
+++ examples/Stokes/ConcentrationOptimization/geometry_kernel.py
@@ -2,6 +2,8 @@ import numpy as np
 import splinepy as sp
 from splinepy.utils.data import cartesian_product as _cartesian_product
 
+from sulzer_inverse import SulzerSMXInverse
+
 EPS = 1e-8
 BOX_LENGTH = 0.14
 BOX_HEIGHT = 0.04
@@ -36,6 +38,7 @@ class SMXKernel:
         self._forerun_thickness = forerun_thickness
         self._parameter_spline_initial = parameter_spline_initial
         self._macro_spline_initial = macro_spline_initial
+        self._tile = SulzerSMXInverse()
 
         self.interfaces = None
         self.multipatch = None
@@ -147,15 +150,29 @@ class SMXKernel:
             self._parametric_grid_points
         )
         # start_grid_points_physical = grid_points_physical[self._tile_start_grid_indices])
-
+        
+        dummy_tile = sp.helpme.create.box(1,1,1)
+        tile_evaluation_points = SulzerSMXInverse._evaluation_points
+        
         for tile_start_grid_index in self._tile_start_grid_indices:
+            # Compute the tile's corner points in physical space
             tile_corner_points_indices = (
                 tile_point_indices_stencil + tile_start_grid_index
             )
             tile_corner_points = grid_points_physical[
                 tile_corner_points_indices
             ]
-            print(tile_corner_points)
+            # Evaluate the parameters
+            tile_corner_points_parametric = self._parametric_grid_points[tile_corner_points_indices]
+            # Get the evaluation points in the parametric domain of the parameter spline
+            dummy_tile.cps = tile_corner_points_parametric
+            parameters_evaluation_points_parametric = dummy_tile.evaluate(tile_evaluation_points)
+            # Evaluate parameter spline on tile
+            tile_parameters = parameter_spline_initial.evaluate(parameters_evaluation_points_parametric)
+            # Evaluate tile
+            tile_patches, _ = self._tile.create_tile(tile_parameters)
+            sp.Multipatch(tile_patches).show()
+            
 
         raise ValueError()
 
